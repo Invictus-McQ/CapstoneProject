370db95c4a917b07907c1cffcdf88b914e80a7cd
Invictus-McQ <mcckm.kvn@gmail.com>
2018-01-01 14:18:03 -0600

    Add files via upload

--- Task 1.R
+++ Task 1.R
@@ -67,9 +67,9 @@ if (!file.exists(paste(outDir, unzipDir, US_blogs_file, sep =""))) {
 if (!file.exists(paste(outDir, unzipDir, US_news_file, sep =""))) {
   unzip(FileName,exdir=outDir)}
 
-US_twitter_Data <- readLines(paste(outDir, "/final/en_US/en_US.twitter.txt", sep =""))
-US_blogs_Data <- readLines(paste(outDir, "/final/en_US/en_US.blogs.txt", sep =""))
-US_news_Data <- readLines(paste(outDir, "/final/en_US/en_US.news.txt", sep =""))
+US_twitter_Data <- readLines(paste(outDir, unzipDir, US_twitter_file, sep =""))
+US_blogs_Data <- readLines(paste(outDir, unzipDir, US_blogs_file, sep =""))
+US_news_Data <- readLines(paste(outDir, unzipDir, US_news_file, sep =""))
 
 ##Download and unzip the data
 FileURL <- "https://www.freewebheaders.com/wordpress/wp-content/uploads/full-list-of-bad-words-banned-by-google-txt-file.zip"
@@ -87,51 +87,80 @@ max(length(US_news_Data))
 
 # Random subset of the data
 random_twitter <- ceiling(runif(n=50000, min=0, max=length(US_twitter_Data)))
-random_blogs <- ceiling(runif(n=20000, min=0, max=length(US_blogs_Data)))
-random_news <- ceiling(runif(n=20000, min=0, max=length(US_news_Data)))
+random_blogs <- ceiling(runif(n=50000, min=0, max=length(US_blogs_Data)))
+random_news <- ceiling(runif(n=50000, min=0, max=length(US_news_Data)))
 
 # subset
-twitter_random_sub <- twitter[random_twitter]
-blogs_random_sub <- blogs[random_blogs]
-news_random_sub <- news[random_news]
+twitter_random_sub <- US_twitter_Data[random_twitter]
+blogs_random_sub <- US_blogs_Data[random_blogs]
+news_random_sub <- US_news_Data[random_news]
 
+WorkingDir <- getwd()
 subDirTask <- "Task01"
+if (file.exists(subDirTask)) {setwd(file.path(WorkingDir, subDirTask))}  else 
+{dir.create(file.path(WorkingDir,subDirTask), showWarnings = FALSE) 
+  setwd(file.path(WorkingDir, subDirTask))}
 
-if (file.exists(subDirTask)) {setwd(file.path(mainDir, subDirTask))}  else 
-{dir.create(file.path(mainDir,subDir3), showWarnings = FALSE) 
-  setwd(file.path(mainDir, subDir3))}
+SubDir <- getwd()
+subDirFolderName <- "US_Subsetted_Outputs"
+if (file.exists(subDirFolderName)) {setwd(file.path(SubDir, subDirFolderName))}  else 
+{dir.create(file.path(SubDir,subDirFolderName), showWarnings = FALSE) 
+  setwd(file.path(SubDir, subDirFolderName))}
 
-# export the data
-write.table(twitter, file='Task1/output/twitter.txt', row.names=FALSE, col.names=FALSE)
-write.table(news, file='Task1/output/news.txt', row.names=FALSE, col.names=FALSE)
-write.table(blogs, file='Task1/output/blogs.txt', row.names=FALSE, col.names=FALSE)
+SubDir <- getwd()
+
+
+file <- "twitter.txt"
+# export the subsetted data
+write.table(random_twitter, file= paste(SubDir, file, sep ='/') , row.names=FALSE, col.names=FALSE)
+file <- "blogs.txt"
+write.table(blogs_random_sub, file= paste(SubDir, file, sep ='/') , row.names=FALSE, col.names=FALSE)
+file <- "news.txt"
+write.table(news_random_sub, file= paste(SubDir, file, sep ='/') , row.names=FALSE, col.names=FALSE)
 # load the random lines
-twitter <- readLines('Task1/output/twitter.txt')
-news <- readLines('Task1/output/news.txt')
-blogs <- readLines('Task1/output/blogs.txt')
-
-str(US_twitter_Data)
-str(US_blogs_Data)
-str(US_news_Data)
-corpora <- data.frame(data_source = factor("twitter"), sentence=US_twitter_Data, words=stri_count_words(US_twitter_Data), 
-                                      bytes = stri_numbytes(US_twitter_Data),
-                                      words_per_byte = stri_numbytes(US_twitter_Data) / stri_count_words(US_twitter_Data))
-summary(corpora)
-nrow(corpora)
-corpora <- rbind(corpora, data.frame(data_source=factor("blogs"), sentence=US_blogs_Data, words=stri_count_words(US_blogs_Data), 
-                                     bytes = stri_numbytes(US_blogs_Data), 
-                                     words_per_byte = stri_numbytes(US_blogs_Data) / stri_count_words(US_blogs_Data)) )
-summary(corpora)
-nrow(corpora)
-corpora <- rbind(corpora, data.frame(data_source=factor("news"),  sentence=US_news_Data, words=stri_count_words(US_news_Data),
-                                     bytes = stri_numbytes(US_news_Data), 
-                                     words_per_byte = stri_numbytes(US_news_Data) / stri_count_words(US_news_Data)) )
-summary(corpora)
-str(corpora)
-nrow(corpora)
-length(corpora)
-head(corpora)
-aggdata <-aggregate(corpora, by= list(as.factor(corpora$words_per_byte)), 
-                    FUN=mean, na.rm=TRUE)
+list.files()
+file <- "twitter.txt"
+twitter <- readLines(paste(SubDir, file, sep ='/'))
+file <- "blogs.txt"
+blogs <- readLines(paste(SubDir, file, sep ='/'))
+file <- "news.txt"
+news <- readLines(paste(SubDir, file, sep ='/'))
 
 
+str(twitter)
+str(blogs)
+str(news)
+
+
+corpus <- data.frame(data_source = factor("twitter"), sentence=twitter, words=stri_count_words(twitter), 
+                                      bytes = stri_numbytes(twitter),
+                                      words_per_byte = stri_numbytes(twitter) / stri_count_words(twitter))
+summary(corpus)
+nrow(corpus)
+corpus <- rbind(corpus, data.frame(data_source=factor("blogs"), sentence=blogs, words=stri_count_words(blogs), 
+                                     bytes = stri_numbytes(blogs), 
+                                     words_per_byte = stri_numbytes(blogs) / stri_count_words(blogs)) )
+summary(corpus)
+nrow(corpus)
+corpus <- rbind(corpus, data.frame(data_source=factor("news"),  sentence=news, words=stri_count_words(news),
+                                     bytes = stri_numbytes(news), 
+                                     words_per_byte = stri_numbytes(news) / stri_count_words(news)) )
+summary(corpus)
+str(corpus)
+nrow(corpus)
+desc(corpus)
+length(corpus)
+head(corpus)
+observations <- corpus$words
+aggdata <-aggregate(x = observations, by= list(unique.values = corpus$data_source) , 
+                    FUN=function(observations){
+                      fitdistr(observations, 
+                               densfun = "Poisson")$estimate
+                    })
+aggdata
+?fitdistr
+
+fitdistr(observations, densfun = "Poisson")
+
+hist(observations, 100)
+library(MASS) 
